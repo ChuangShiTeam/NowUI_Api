fc4dc648aa11b249529e50d8a26518500a5e92ec
marcus <marcus_hualebin@163.com>
2017-11-26 12:21:46 +0800

    完善

--- src/main/java/io/chuangshi/user/controller/UserController.java
+++ src/main/java/io/chuangshi/user/controller/UserController.java
@@ -1,8 +1,15 @@
 package io.chuangshi.user.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.baomidou.mybatisplus.plugins.Page;
+
+import io.chuangshi.user.entity.User;
+import io.chuangshi.user.service.UserService;
 
 /**
  * <p>
@@ -15,6 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 分页 PAGE
+     */
+    @GetMapping("/test")
+    public Page<User> test() {
+        return userService.selectPage(new Page<User>(0, 12));
+    }
     
 
 }
